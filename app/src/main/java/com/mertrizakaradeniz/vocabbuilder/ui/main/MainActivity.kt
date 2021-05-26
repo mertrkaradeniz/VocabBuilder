2bc71c3d4db7fe15b1a5238f6e5c17515cb426bb
Mert KARADENIZ <17050111054@ybu.edu.tr>
2021-05-26 14:20:10 +0000

    Bottom navigation setup

--- app/src/main/java/com/mertrizakaradeniz/vocabbuilder/ui/main/MainActivity.kt
+++ app/src/main/java/com/mertrizakaradeniz/vocabbuilder/ui/main/MainActivity.kt
@@ -2,11 +2,49 @@ package com.mertrizakaradeniz.vocabbuilder.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.mertrizakaradeniz.vocabbuilder.R
+import com.mertrizakaradeniz.vocabbuilder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.wordListFragment,
+                R.id.memorizeWordFragment,
+                R.id.wordQuizFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
